2a93e88dda46ae994c36677f0fa3dc9c389eaf8c
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-08-04 11:20:46 +0300

    using helm directly

--- shell/local-ci.sh
+++ shell/local-ci.sh
@@ -3,5 +3,4 @@ set -e
 PROJ=identity-connector
 IMAGE=debezium/connect:0.8.0.Final
 kubectl config use-context minikube
-
-./helm-deploy.sh -i $IMAGE -r $PROJ -c ../kube/postgres-connector
+helm upgrade --install --set image=$IMAGE $PROJ ../kube/postgres-connector
