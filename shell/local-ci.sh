a814fb81e8e46627a5ee4190be5921233643c058
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-08-04 22:46:39 +0300

    adding identity-compactor

--- shell/local-ci.sh
+++ shell/local-ci.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-PROJ=identity-connector
-IMAGE=debezium/connect:0.8.0.Final
 kubectl config use-context minikube
-helm upgrade --install --set image=$IMAGE $PROJ ../kube/postgres-connector
+helm upgrade --install identity-connector ../kube/postgres-connector
+helm upgrade --install identity-compactor ../kube/compactor
