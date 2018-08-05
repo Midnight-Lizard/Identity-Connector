5bba4f9dbe767dab18c972936c2b36eae2884c4c
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-08-05 13:46:59 +0300

    compacting AspNetUsers table

---
+++ kube/compactor/queries/AspNetUsers.sql
@@ -0,0 +1,29 @@
+CREATE STREAM RawStream_AspNetUsers (
+  after STRUCT<
+    Id VARCHAR,
+    UserName VARCHAR,
+    DisplayName VARCHAR,
+    NormalizedUserName VARCHAR,
+    Email VARCHAR,
+    NormalizedEmail VARCHAR,
+    EmailConfirmed BOOLEAN,
+    PhoneNumber VARCHAR,
+    PhoneNumberConfirmed BOOLEAN>)
+  WITH (
+    KAFKA_TOPIC = 'iddb.public.AspNetUsers',
+    VALUE_FORMAT = 'JSON');
+
+CREATE TABLE Table_AspNetUsers WITH (
+    KAFKA_TOPIC = 'ksql-table_iddb.public.AspNetUsers',
+    VALUE_FORMAT = 'JSON') AS
+  SELECT
+    after->Id as Id,
+    after->UserName as UserName,
+    after->DisplayName as DisplayName,
+    after->NormalizedUserName as NormalizedUserName,
+    after->Email as Email,
+    after->NormalizedEmail as NormalizedEmail,
+    after->EmailConfirmed as EmailConfirmed,
+    after->PhoneNumber as PhoneNumber,
+    after->PhoneNumberConfirmed as PhoneNumberConfirmed
+  FROM RawStream_AspNetUsers;
